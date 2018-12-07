705681ed55e75240c340851a7342a58804034afe
김보하 <rlaqhgk9412@naver.com>
2018-12-08 04:02:18 +0900

    Trex Jump
    - Animation
    - Controller

---
+++ Assets/Scripts/TrexController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrexController : MonoBehaviour
+{
+    [SerializeField, Range(0, 1000)] private float jumpHeight;
+
+    private Rigidbody2D trexRigid;
+
+    private void Awake()
+    {
+        trexRigid = this.GetComponentInChildren<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            TrexJump();
+        }
+    }
+
+    private void TrexJump ()
+    {
+        trexRigid.AddForce(Vector3.up * jumpHeight);
+    }
+}
